4a7d4ec3075570d1c1576a45aea9d164b263e693
zookao <caozongchao@163.com>
2026-08-26 13:52:30 +0800

    logrotate

--- docker/logrotate.sh
+++ docker/logrotate.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# 每天北京时间午夜轮转 supervisor 日志（由 supervisord 拉起，崩溃自动重启）
+# 按大小轮转 supervisor 日志（单文件超 70M 触发），每分钟检查一次
 # logrotate 用 copytruncate 清空原文件，supervisor 无需重开文件句柄、进程无需重启
 set -e
 while true; do
-    now=$(TZ=Asia/Shanghai date +%s)
-    midnight=$(TZ=Asia/Shanghai date -d "tomorrow 00:00:00" +%s)
-    sleep $((midnight - now))
     /usr/sbin/logrotate -s /tmp/logrotate-supervisor.state /etc/logrotate.d/supervisor
+    sleep 60
 done
